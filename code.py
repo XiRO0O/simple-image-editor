6b3f8186e233675c3079477d41f5ba40a2e934ab
ONKJUNUA <jaseko2002@gmail.com>
2022-06-18 13:27:57 +0200

    working editor

--- code.py
+++ code.py
@@ -1,10 +1,21 @@
 import PySimpleGUI as sg
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageOps
 from io import BytesIO
 
 def update_image(original,blur,contrast,emboss,contour,flipx,flipy):
+    global image
     image = original.filter(ImageFilter.GaussianBlur(blur))
     image = image.filter(ImageFilter.UnsharpMask(contrast))
+
+    if emboss:
+        image = image.filter(ImageFilter.EMBOSS())
+    if contour:
+        image = image.filter(ImageFilter.CONTOUR())
+
+    if flipx:
+        image = ImageOps.mirror(image)
+    if flipy:
+        image = ImageOps.flip(image)
 
     bio = BytesIO()
     image.save(bio, format = 'PNG')
@@ -39,4 +50,7 @@ while True:
         values['-FLIPX-'],
         values['-FLIPY-'])
 
+    if event == '-SAVE-':
+        save_path = sg.popup_get_file('Save', save_as = True, no_window = True) + '.png'
+        image.save(save_path,'PNG')
 window.close()
